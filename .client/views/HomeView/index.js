d15095406ae8e31a1ee7dc811fb71c2ab68ac5e2
okmttdhr <10101114fin@gmail.com>
2015-11-02 21:04:07 +0900

    [add]ReactMeteorいれた

--- .client/views/HomeView/index.js
+++ .client/views/HomeView/index.js
@@ -24,26 +24,35 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export class HomeView extends React.Component {
-  static propTypes = {
+export const HomeView = React.createClass({
+  propTypes: {
     actions: React.PropTypes.object,
     todos: React.PropTypes.array,
     counter: React.PropTypes.number,
-  }
+  },
+
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      todos: Todos.find({}).fetch(),
+    };
+  },
 
   render () {
     console.log(this.props);
     return (
       <div className='container text-center'>
         <h1>Welcome to the React Redux Starter Kit</h1>
-        <button onClick={this.props.actions.increment}>Increment</button>
+        <button onClick={this.props.actions.increment}>Incrementttttttt</button>
         {this.props.counter}
+        {JSON.stringify(this.data.todos)}
         <TodoApp
           todos={this.props.todos}
           actions={this.props.actions}/>
       </div>
     );
-  }
-}
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeView);
