5b2e38cf14508c6b1b1562521e08bf9d84e43862
okmttdhr <10101114fin@gmail.com>
2015-11-04 14:40:41 +0900

    [add]reducerリファクタ

--- .client/reducers/index.js
+++ .client/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerStateReducer } from 'redux-router';
-import todos from './Todos';
+import todo from './Todo';
 import counter from './counter';
 
 export default combineReducers({
   router: routerStateReducer,
-  todos,
+  todo,
   counter,
 });
